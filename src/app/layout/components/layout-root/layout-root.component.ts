4a62af0261ee2112eae2b3c6c17d724bdfd47b9c
Jannik Lipke <Jannik.Lipke@gmx.de>
2022-03-10 00:32:55 +0100

    Better Styles

--- src/app/layout/components/layout-root/layout-root.component.ts
+++ src/app/layout/components/layout-root/layout-root.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-layout-root',
   templateUrl: './layout-root.component.html',
-  styleUrls: ['./layout-root.component.scss']
+  styleUrls: ['./layout-root.component.scss'],
 })
 export class LayoutRootComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
